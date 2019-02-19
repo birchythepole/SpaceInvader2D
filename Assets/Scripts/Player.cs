a7d752caa50be6c91634765e1ee8cb0f26535481
BirchyThePole <birchythepole@gmail.com>
2019-02-19 23:46:34 +0100

    Added Time.deltaTime for framerate independent movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
+    [SerializeField] [Range(1, 30)] float moveSpeed = 10f;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,10 +14,18 @@ public class Player : MonoBehaviour {
 	void Update () {
         Move();
 	}
-
+    /*Dela.time jest funkcja, ktora powoduje iz poruszanie sie obiektu w Unity jest
+     niezalezne od ilosci klatek (frame rate independent), jest to bardzo potrzebna
+     funkcja gdyz sprawia ze gra ma caly czas taka sama predkosc, niezaleznie od
+     predkosci komputera na ktorym gramy. Delta time mowi nam ile trwa wygenerowanie
+     pojedynczej klatki, i jezeli pomnozmy to przez nasza wartosc, za kazdym razem
+     otrzymamy taka sama predkosc niezaleznie od liczby klatek
+     FPS 10, Duration of frame 0.1s, Distance per second, 1 x 10 x 0.1 = 1 
+     FPS 100, Duration of frame 0.01s, Distance per second, 1 x 10 x 0.01 = 1 
+         */
     private void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal"); // Zmienna pobierajaca os pozioma z Unity, i zmiana jaka robimy
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; // Zmienna pobierajaca os pozioma z Unity, i zmiana jaka robimy
         var newXPos = transform.position.x + deltaX; // Zmienna przechowujaca wartosc o jaka zmieniamy polozenie gracza, polozenie plus zmiana polozenia
         transform.position = new Vector2(newXPos, transform.position.y); //formula ktora przesowa gracza (przesowa jego os Y) o wartosc newXPos, zmienia polozenie i mowi zostan tu, w osi Y
     }
