f3e2aa4831d22cb83881cd9b13589066aa5fbae1
BirchyThePole <birchythepole@gmail.com>
2019-02-19 22:58:28 +0100

    Added assets, setup scene, creted Player and enemy object, and script for player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal"); // Zmienna pobierajaca os pozioma z Unity, i zmiana jaka robimy
+        var newXPos = transform.position.x + deltaX; // Zmienna przechowujaca wartosc o jaka zmieniamy polozenie gracza, polozenie plus zmiana polozenia
+        transform.position = new Vector2(newXPos, transform.position.y); //formula ktora przesowa gracza (przesowa jego os Y) o wartosc newXPos, zmienia polozenie i mowi zostan tu, w osi Y
+    }
+}
