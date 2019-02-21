182d4a2168455c93cd7979db28fe753c666aaf70
BirchyThePole <birchythepole@gmail.com>
2019-02-21 13:56:47 +0100

    Added Shredder that destroys laser instances

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+    /* Metoda Publicznia ktora powoduje, że przy zetknieciu z jakims
+     Obiektem Gry obiekt ten jest niszczony*/
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
