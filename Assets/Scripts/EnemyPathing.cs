4bacee59c65b8a2e5741542ab44ce6f21095757d
BirchyThePole <birchythepole@gmail.com>
2019-02-21 14:32:23 +0100

    Created waypoints and prefabed it, than created pathing script with list in it and assigned waypoints to list in prefabs

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+    /*Lista stworzona do przechowywania waypointow z poziomy edytora*/
+    [SerializeField]  List<Transform> waypoints; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
