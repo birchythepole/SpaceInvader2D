d667b3ca5150785c53fc188c806dc5b53b64b2fc
BirchyThePole <birchythepole@gmail.com>
2019-02-25 15:51:58 +0100

    Created method in EnemyPathing.cs for enemy movment towards waypoints

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour {
 
     /*Lista stworzona do przechowywania waypointow z poziomy edytora*/
-    [SerializeField]  List<Transform> waypoints; 
+    [SerializeField]  List<Transform> waypoints;
+    [SerializeField] [Range(0, 10)] float moveSpeed = 2f; // Deklarowana prędkość przeciwnika
+
+    int waypointIndex = 0;
 	// Use this for initialization
 	void Start () {
+        // ustawienie poczatkowej pozycji przeciwnika na pozycje pierwszego waypointa
+        transform.position = waypoints[waypointIndex].transform.position;
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        
+        Move();
+    }
+
+    private void Move()
+    {
+        /*Metoda która jeżeli numer indeksu listy do ktorej przyczepione sa waypointy
+         jest mniejsz lub rowny ilosci tych punkow wykonuje ruch wzdloz punktow
+         w przeciwnym wypadku niszczy obiekt*/
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            /*Deklarujemy zmienna ktora bedzie przechowywac wartosc wspolzednych nastepnego
+             waypointa do ktorego bedziemy sie kierowac*/
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            /*Deklarujemy zmienna ktora bedzie pzechowywac wartosc predkosci przeciwnika 
+             pomnożoną przez funkcje Time.delta time tak aby prędkość obiektu była
+             framerate independent*/
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            /* W aktualnej pozycj obiektu wywołujemy funkcje MoveTowards w Vector2 tak aby 
+             * obiekt poruszał sie w kierunku nastpnego waypointa, jako paametry podajemy
+             * aktualna pozycje, pozycje celu oraz predkosc przeciwnika */
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            /* Na konec jezeli pozycja jest rowna pozycji celu inkrementujemy indeks listy aby obiekt
+             przemieszczal sie w kierunku kolejnego celu*/
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
