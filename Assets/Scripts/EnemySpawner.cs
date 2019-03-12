fb39adf10921de3991e78f55a8ef2d83968ae73b
BirchyThePole <birchythepole@gmail.com>
2019-03-12 09:04:35 +0100

    Created coroutine loops for sapwning multply enemy waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 	// Use this for initialization
 	void Start () {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllEnemyWaves());
 	}
 	
 	private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
@@ -20,6 +19,14 @@ public class EnemySpawner : MonoBehaviour {
                 Quaternion.identity);
             newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+    private IEnumerator SpawnAllEnemyWaves()
+    {
+        for(int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
         }
     }
 }
